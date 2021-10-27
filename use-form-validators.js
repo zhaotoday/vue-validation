682b815b9ca732543a11e68f5e1654714d253ee7
zhaojintian <729234283@qq.com>
2021-10-27 15:09:56 +0800

    u

--- use-form-validators.js
+++ use-form-validators.js
@@ -1,6 +1,6 @@
 import AsyncValidator from "async-validator";
 
-export const useFormValidators = () => {
+export const useFormValidators = ({ ruleOptions = {} } = {}) => {
   return {
     isUserId({ message = "" } = {}) {
       return {
@@ -8,36 +8,42 @@ export const useFormValidators = () => {
           return !value || /^[1-9]\d{7}$/.test(value);
         },
         message: message || "请输入8位数字ID",
+        ...ruleOptions,
       };
     },
     isAccount({ label = "账号", message = "" } = {}) {
       return {
         pattern: /[a-zA-Z0-9_-]{8,20}/,
         message: message || `${label}格式错误`,
+        ...ruleOptions,
       };
     },
     isNickName({ label = "昵称", message = "" } = {}) {
       return {
         pattern: /^[\\w\u4e00-\u9fa5]{2,8}$/,
         message: message || `${label}格式错误`,
+        ...ruleOptions,
       };
     },
     isRequired({ label, message } = {}) {
       return {
         required: true,
         message: message || `请输入${label}`,
+        ...ruleOptions,
       };
     },
     isPhoneNumber({ label = "手机号", message = "" } = {}) {
       return {
         pattern: /^1\d{2}\s?\d{4}\s?\d{4}$/,
         message: message || `${label}格式错误`,
+        ...ruleOptions,
       };
     },
     isEmail({ label = "邮箱", message = "" } = {}) {
       return {
         type: "email",
         message: message || `${label}格式错误`,
+        ...ruleOptions,
       };
     },
     isPhoneNumberOrEmail({ label = "账号", message = "" } = {}) {
@@ -50,18 +56,21 @@ export const useFormValidators = () => {
           return phoneNumberPattern.test(value) || emailPattern.test(value);
         },
         message: message || `${label}格式错误（必须是手机号或邮箱）`,
+        ...ruleOptions,
       };
     },
     isPassword({ label = "密码", message = "" } = {}) {
       return {
         pattern: /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/,
         message: message || `${label}格式错误`,
+        ...ruleOptions,
       };
     },
     isCaptcha({ label = "验证码", message = "" } = {}) {
       return {
         len: 6,
         message: message || `${label}格式错误`,
+        ...ruleOptions,
       };
     },
     async validate(cForm, field, callback) {
